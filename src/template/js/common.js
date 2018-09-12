b9e22e47180a0d07d3c11bc9c8ec2fc77a054f20
e5ash <e5ash.bro@gmail.com>
2018-09-12 20:19:34 +0300

    add

---
+++ src/template/js/common.js
@@ -0,0 +1,5 @@
+$(document).ready(function($) {
+
+	$('.input_phone .input__wrap').mask('+7 (000) 000-00-00');
+
+});
--- src/template/scripts/common.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function($) {
-
-	$('.input_phone .input__wrap').mask('+7 (000) 000-00-00');
-
-});
